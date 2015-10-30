225c638d0742508e732cc1daecf9dbf1ef114d5e
Eddy Hintze <eddy.hintze@gmail.com>
2015-10-30 06:54:50 +0100

    Removed local _set_url_header()

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class Xkcd(CustomUtils):
         self._restart = restart
 
         # Set url_header
-        self._url_header = self._set_url_header(url_header)
+        self._url_header = self.set_url_header(url_header)
 
         # If we have proxies then add them
         if len(proxies) > 0:
@@ -105,13 +105,6 @@ class Xkcd(CustomUtils):
 
         # Everything was successful
         return True
-
-    def _set_url_header(self, url_header):
-        if url_header is None:
-            # Use default from CustomUtils
-            return self.get_default_header()
-        else:
-            return url_header
 
     def _save_meta_data(self, data):
         xkcd_data = Data(num=data['num'],
